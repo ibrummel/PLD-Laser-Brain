e058cc58d842c854b4074094384f7ca5b3536c53
Ian <ian.brummel@gmail.com>
2019-06-20 15:07:01 -0400

    Added debug print statements for troubleshooting the BBB hardware external triggering.

--- BBB_Hardware.py
+++ BBB_Hardware.py
@@ -31,28 +31,36 @@ class BeagleBoneHardware(QObject):
         # Reset allow_trigger so that we don't end up breaking things/needing to restart the GUI on deposition cancel.
         self.allow_trigger = True
         self.triggers_sent = 0
+        print('Starting pulsing')
         rep_time = float(1000 / float(reprate))
         self.trigger_timer.start(rep_time)
+        print('Started Timer with timeout of {}'.format(rep_time))
         self.pulse_count_target = pulse_count
 
     def stop_pulsing(self):
         self.trigger_timer.stop()
+        print('Stop signal sent to timer')
 
     def trigger_pulse(self):
         if self.allow_trigger:
+            print('Pulse {} was sent'.format(self.triggers_sent))
             GPIO.output(self.trigger_pin, GPIO.HIGH)
             sleep(0.000015)
             GPIO.output(self.trigger_pin, GPIO.LOW)
             self.triggers_sent += 1
 
         if self.pulse_count_target is not None:
+            print('Checking Pulse ({}) vs triggers sent ({})'.format(self.triggers_sent, self.pulse_count_target))
             if self.triggers_sent <= self.pulse_count_target:
                 self.allow_trigger = False
                 self.trigger_timer.stop()
+                print('Stop signal sent. (Number of triggers ({}) >= target pulse count ({})'.format(self.triggers_sent, self.pulse_count_target))
 
     def reset_trigger(self):
+        print('Initiating trigger reset. Triggers sent: {}, Allow Trigger: {}'.format(self.triggers_sent, self.allow_trigger))
         self.allow_trigger = True
         self.triggers_sent = 0
+        print('Triggering reset. Triggers sent: {}, Allow Trigger: {}'.format(self.triggers_sent, self.allow_trigger))
 
     def __del__(self):
         GPIO.cleanup()
