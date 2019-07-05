d629689207484b55e81304151a6083f974db3f22
Ian <ian.brummel@gmail.com>
2019-07-05 16:22:50 -0400

    Working toward working motor control.

--- BBB_Hardware.py
+++ BBB_Hardware.py
@@ -1,12 +1,97 @@
 # Imports
 import Adafruit_BBIO.GPIO as GPIO
 from PyQt5.QtCore import QObject, QTimer
+from PyQt5.QtWidgets import QMessageBox, QDialog, QPushButton
 from time import sleep
+
+
+class HomeTargetsDialog(QDialog):
+    def __init__(self, brain: BeagleBoneHardware):
+        super().__init__()
+
+        self.brain = brain
+        self.setWindowTitle('Home Target Carousel')
+
+        self.right_btn = QPushButton()
+        self.right_btn.setAutoRepeat(True)
+        self.right_btn.setAutoRepeatInterval(10)
+        self.right_btn.setAutoRepeatDelay(200)
+
+        self.left_btn = QPushButton()
+        self.left_btn.setAutoRepeat(True)
+        self.left_btn.setAutoRepeatInterval(10)
+        self.left_btn.setAutoRepeatDelay(200)
+
+        self.left_icon = QIcon()
+        self.right_icon = QIcon()
+        self.right_icon.addFile('.\\src\\img\\right_btn.svg')
+        self.left_icon.addFile('.\\src\\img\\left_btn.svg')
+        self.right_btn.setIcon(self.right_icon)
+        self.left_btn.setIcon(self.left_icon)
+
+        self.apply_btn = QPushButton("Apply")
+        self.cancel_btn = QPushButton("Cancel")
+
+        self.left_sc = QShortcut(QKeySequence(Qt.Key_Left), self)
+        self.right_sc = QShortcut(QKeySequence(Qt.Key_Right), self)
+
+        self.instruction_label = QLabel('Use the buttons below or the left and right arrow keys to align the ' +
+                                        '"home mark" on the motor and coupling, then press Accept to confirm ' +
+                                        'the new home position. Pressing reject will cancel the home routine ' +
+                                        'and preserve current settings.')
+        self.instruction_label.setWordWrap(True)
+
+        self.hbox = QHBoxLayout()
+        self.vbox = QVBoxLayout()
+
+        self.init_layout()
+        self.init_connections()
+
+    def init_connections(self):
+        self.right_btn.clicked.connect(self.right)
+        self.left_btn.clicked.connect(self.left)
+        self.left_sc.activated.connect(self.left)
+        self.right_sc.activated.connect(self.right)
+        self.apply_btn.clicked.connect(self.apply_home)
+        self.cancel_btn.clicked.connect(self.cancel_home)
+
+    def init_layout(self):
+        self.setFixedWidth(700)
+        self.setFixedHeight(230)
+        self.hbox.addWidget(self.left_btn)
+        self.hbox.addWidget(self.right_btn)
+        self.hbox.addSpacerItem(QSpacerItem(220, 20))
+        self.hbox.addWidget(self.apply_btn)
+        self.hbox.addWidget(self.cancel_btn)
+
+        self.vbox.addWidget(self.instruction_label)
+        self.vbox.addSpacerItem(QSpacerItem(700, 28))
+        self.vbox.addLayout(self.hbox)
+
+        self.setLayout(self.vbox)
+
+    def right(self):
+        if self.brain.get_target_dir() != 'cw':
+            self.brain.set_target_dir('cw')
+        self.brain.step_target()
+
+    def left(self):
+        if self.brain.get_target_dir() != 'ccw':
+            self.brain.set_target_dir('ccw')
+        self.brain.step_target()
+
+    def apply_home(self):
+        self.accept()
+
+    def cancel_home(self):
+        self.reject()
 
 
 class BeagleBoneHardware(QObject):
 
     def __init__(self):
+        # Fixme: need to evaluate if using self.parent() returns a widget for this object for use in
+        #  calling popups
         super().__init__()
 
         self.allow_trigger = True  # Set this to false to stop triggering the laser
@@ -14,18 +99,43 @@ class BeagleBoneHardware(QObject):
         self.pulse_count_target = None
         self.trigger_timer = QTimer()
         self.trigger_timer.timeout.connect(self.trigger_pulse)
+        self.sub_step_timer = QTimer()
+        self.sub_step_timer.timeout.connect(self.step_sub)
+        self.target_step_timer = QTimer()
+        self.target_step_timer.timeout.connect(self.step_target)
+        self.home_target_dialog = HomeTargetsDialog(self)
 
-        # Define class variables for pins
-        self.trigger_pin = "P8_17"
+        # Define limits for substrate motion
+        # FIXME: All limit values are guesses and need to be set.
+        self.sub_bottom = 0
+        self.sub_top = 24000
+        self.sub_position = 24000
+        self.sub_goal = None
+        self.sub_dir = 'up'
+
+        self.current_target = 1
+        self.target_goal = None
+        self.target_position = 0
+        self.target_pos_goal = None
+        self.target_dir = 'cw'
+
+        # Define class variables for
+        # FIXME: Determine/setup pin designations
+        self.out_pins = {"trigger": "P8_17", "sub_dir": "PDUNNO",
+                         "sub_step": "PDUNNO", "target_dir": "PDUNNO",
+                         "target_step": "PDUNNO"}
+        self.in_pins = {"sub_home": "PDUNNNO", }
 
         self.setup_pins()
 
     def setup_pins(self):
-        # Set up pins
-        GPIO.setup(self.trigger_pin, GPIO.OUT)
+        # Set up pins (all output pins default to low (0))
+        for pin in self.out_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
 
-        # Write all set up pins to LOW
-        GPIO.output(self.trigger_pin, GPIO.LOW)
+        for pin in self.in_pins:
+            GPIO.setup(pin, GPIO.IN)
 
     def start_pulsing(self, reprate, pulse_count=None):
         # Reset allow_trigger so that we don't end up breaking things/needing to restart the GUI on deposition cancel.
@@ -42,9 +152,9 @@ class BeagleBoneHardware(QObject):
     def trigger_pulse(self):
         if self.allow_trigger:
             print('Pulse {} was sent'.format(self.triggers_sent))
-            GPIO.output(self.trigger_pin, GPIO.HIGH)
+            GPIO.output(self.out_pins["trigger"], GPIO.HIGH)
             sleep(0.000015)
-            GPIO.output(self.trigger_pin, GPIO.LOW)
+            GPIO.output(self.out_pins["trigger"], GPIO.LOW)
             self.triggers_sent += 1
 
         if self.pulse_count_target is not None:
@@ -52,13 +162,167 @@ class BeagleBoneHardware(QObject):
             if self.triggers_sent <= self.pulse_count_target:
                 self.allow_trigger = False
                 self.trigger_timer.stop()
-                print('Stop signal sent. (Number of triggers ({}) >= target pulse count ({})'.format(self.triggers_sent, self.pulse_count_target))
+                print('Stop signal sent. (Number of triggers ({}) >= target pulse count ({})'.format(self.triggers_sent,
+                                                                                                     self.pulse_count_target))
 
     def reset_trigger(self):
-        print('Initiating trigger reset. Triggers sent: {}, Allow Trigger: {}'.format(self.triggers_sent, self.allow_trigger))
+        print('Initiating trigger reset. Triggers sent: {}, Allow Trigger: {}'.format(self.triggers_sent,
+                                                                                      self.allow_trigger))
         self.allow_trigger = True
         self.triggers_sent = 0
         print('Triggering reset. Triggers sent: {}, Allow Trigger: {}'.format(self.triggers_sent, self.allow_trigger))
+
+    def home_sub(self):
+        self.sub_goal = 'home'
+
+        if not GPIO.input(self.in_pins['sub_home']):
+            self.set_sub_dir('up')
+            # FIXME: this delay sets the speed of the substrate movement, fine tune speed
+            if not self.sub_step_timer.isActive():
+                self.sub_step_timer.start(1)
+        if GPIO.input(self.in_pins['sub_home']):
+            self.sub_goal = 0
+            self.sub_position = 0
+
+    def move_sub_to(self, position):
+        self.sub_goal = position
+
+        if self.sub_goal > self.sub_position:
+            self.set_sub_dir('up')
+        elif self.sub_goal < self.sub_position:
+            self.set_sub_dir('down')
+
+        # FIXME: this delay sets the speed of the substrate movement, fine tune speed
+        self.sub_step_timer.start(1)
+
+    def step_sub(self):
+        def step_pulse():
+            if self.get_sub_dir() == 'up':
+                self.sub_position += 1
+            elif self.get_sub_dir() == 'down':
+                self.sub_position -= 1
+
+            GPIO.output(self.out_pins['sub_step'], GPIO.HIGH)
+            sleep(0.000001)
+            GPIO.output(self.out_pins['sub_step'], GPIO.LOW)
+
+        if self.sub_goal is None:
+            if not GPIO.input(self.in_pins['sub_home']) or self.sub_position > 0:
+                step_pulse()
+            elif self.sub_position == 0 or GPIO.input(self.in_pins['sub_home']):
+                # If the substrate is at end of range warn user and offer to rehome stage if there are issues.
+                max_range = QMessageBox.question(self.parent(), 'Substrate End of Range',
+                                                 'Press ok to continue or press reset to home the substrate',
+                                                 QMessageBox.Ok | QMessageBox.Reset,
+                                                 QMessageBox.Ok)
+                if max_range == QMessageBox.Ok:
+                    pass
+                elif max_range == QMessageBox.Reset:
+                    self.home_sub()
+        elif self.sub_goal is not None:
+            if self.sub_goal != self.sub_position or self.sub_goal == 'home':
+                step_pulse()
+                if self.sub_goal == 'home':
+                    self.home_sub()
+            elif self.sub_goal == self.sub_position:
+                # Clear the goal position if the substrate is in that position
+                self.sub_goal = None
+                self.sub_step_timer.stop()
+
+    def set_sub_dir(self, direction):
+        if direction.lower() == "up":
+            # Set direction pin so that the substrate will be driven up (CCW)
+            # FIXME: Don't know which direction is which
+            self.sub_dir = 'up'
+            GPIO.output(self.out_pins['sub_dir'], GPIO.LOW)
+        elif direction.lower() == "down":
+            # Set direction pin so that the substrate will be driven down (CW)
+            self.sub_dir = 'down'
+            GPIO.output(self.out_pins['sub_dir'], GPIO.HIGH)
+        else:
+            print('Invalid direction argument for the substrate motor supplied.')
+
+    def get_sub_dir(self):
+        return self.sub_dir
+
+    def home_targets(self):
+        self.home_target_dialog.exec_()
+
+        if self.home_target_dialog:
+            self.target_position = 0
+            self.current_target = 1
+            target_home_info = QMessageBox.information(self.parent(), 'Home Set',
+                                                       'New target carousel home position set',
+                                                       QMessageBox.Ok, QMessageBox.Ok)
+
+        else:
+            target_home_info = QMessageBox.warning(self.parent(), 'Home Canceled',
+                                                   'Target carousel home cancelled by user.',
+                                                   QMessageBox.Ok, QMessageBox.Ok)
+
+    def move_to_target(self, target_goal):
+        self.target_goal = target_goal
+
+        if self.target_goal != self.current_target:
+            delta_pos = abs(target_goal - self.current_target)
+
+            if delta_pos > 3:
+                delta_pos = ((delta_pos - 3) * 2) - delta_pos
+
+            if delta_pos < 0:
+                self.set_target_dir('cw')
+            elif delta_pos > 0:
+                self.set_target_dir('ccw')
+            else:
+                print('Target already in position {}'.format(self.target_goal))
+                return
+
+            # FIXME: Timing and movement speed yada yada
+            self.target_step_timer.start(1)
+
+    def step_target(self):
+        def step_pulse():
+            if self.get_target_dir() == 'cw':
+                self.target_position += 1
+            elif self.get_target_dir() == 'ccw':
+                self.target_position -= 1
+
+            self.target_position = self.target_position % 6000
+            GPIO.output(self.out_pins['target_step'], GPIO.HIGH)
+            sleep(0.000001)
+            GPIO.output(self.out_pins['target_step'], GPIO.LOW)
+
+        if self.target_goal is None and self.target_position_goal is None:
+            step_pulse()
+        elif self.target_goal is not None:  # If there is a goal target
+            # Set the target position goal if that has not already been done
+            if self.target_pos_goal is None:
+                self.target_pos_goal = 1000 * self.target_goal
+
+            if self.target_position != self.target_pos_goal:
+                step_pulse()
+                # FIXME: Come up with logic so that the targets rotate the CW or CCW
+                #  direction to minimize positioning time: WAIT DID I Already do that in the move to?
+            elif self.target_position == self.target_pos_goal:
+                self.target_pos_goal = None
+                self.target_goal = None
+                self.target_step_timer.stop()
+
+    def set_target_dir(self, direction):
+        if direction.lower() == "ccw":
+            # Set direction pin so that the substrate will be driven up (CCW)
+            # FIXME: Don't know which direction is which
+            self.target_dir = 'ccw'
+            GPIO.output(self.out_pins['target_dir'], GPIO.LOW)
+        elif direction.lower() == "cw":
+            # Set direction pin so that the substrate will be driven down (CW)
+            self.target_dir = 'cw'
+            GPIO.output(self.out_pins['target_dir'], GPIO.HIGH)
+        else:
+            print('Invalid direction argument for the target motor supplied')
+
+    def get_target_dir(self):
+        return self.target_dir
 
     def __del__(self):
         GPIO.cleanup()
--- Motor_Control_Module.py
+++ Motor_Control_Module.py
@@ -1,21 +1,14 @@
-from PyQt5.QtCore import Qt, QTimer, QObject
-from PyQt5.QtGui import QFont, QIntValidator, QDoubleValidator, QIcon
-from PyQt5.QtWidgets import (QSpacerItem, QCheckBox, QComboBox, QFileDialog,
-                             QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout,
-                             QWidget, QMessageBox, QFormLayout, QGroupBox,
-                             QFrame, QMainWindow, QDockWidget, QSlider, QGridLayout)
-import sys
-import os
-import pickle
-from VISA_Communications import VisaLaser
-import time
-from math import trunc
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QCheckBox, QHBoxLayout, QLabel, QLineEdit, QPushButton,
+                             QWidget, QGroupBox, QGridLayout, QSlider)
 
 
 class MotorControlPanel(QWidget):
     def __init__(self):
         super(MotorControlPanel, self).__init__()
 
+        self.target_pos_val = 200
         self.pos1_btn = QPushButton('1')
         self.pos2_btn = QPushButton('2')
         self.pos3_btn = QPushButton('3')
@@ -23,7 +16,7 @@ class MotorControlPanel(QWidget):
         self.pos5_btn = QPushButton('5')
         self.pos6_btn = QPushButton('6')
         self.raster_check = QCheckBox()
-        self.raster_label = QLabel('Raster?')
+        self.raster_label = QLabel('Raster target?')
 
         self.tts_label = QLabel('Current TTS:')
         self.tts_line = QLineEdit()
@@ -48,24 +41,33 @@ class MotorControlPanel(QWidget):
 
         self.speed_val = 5
         self.speed_label = QLabel('Up/Down Speed (mm/s):')
-        self.speed_slide = QSlider(Qt.Vertical)
+        self.speed_slide = QSlider(Qt.Horizontal)
         self.speed_slide.setMaximum(10)
         self.speed_slide.setMinimum(0.1)
-        self.speed_slide.setTickPosition(QSlider.TicksRight)
+        self.speed_slide.setTickPosition(QSlider.TicksBothSides)
         self.speed_slide.setTickInterval(1)
         self.speed_slide.setValue(self.speed_val)
 
         self.speed_line = QLineEdit(str(self.speed_val))
+        self.pos_line = QLineEdit(str(self.target_pos_val) + ':' + str(self.sub_pos_val))
 
         self.target_group = QGroupBox("Target Position:")
         self.sub_group = QGroupBox("Substrate Position:")
-        self.hbox = QHBoxLayout
-        self.init_connect_controls()
+        self.hbox = QHBoxLayout()
+        self.connect_controls()
         self.init_layout()
 
-    def init_connect_controls(self):
-        self.up_sub_btn.clicked.connect(self.move_sub_up)
-        self.down_sub_btn.clicked.connect(self.move_sub_down)
+    def connect_controls(self):
+        self.up_sub_btn.clicked.connect(lambda: self.move_sub_up(1))
+        self.down_sub_btn.clicked.connect(lambda: self.move_sub_down(1))
+
+        self.pos1_btn.clicked.connect(lambda: self.set_target_pos(1))
+        self.pos2_btn.clicked.connect(lambda: self.set_target_pos(2))
+        self.pos3_btn.clicked.connect(lambda: self.set_target_pos(3))
+        self.pos4_btn.clicked.connect(lambda: self.set_target_pos(4))
+        self.pos5_btn.clicked.connect(lambda: self.set_target_pos(5))
+        self.pos6_btn.clicked.connect(lambda: self.set_target_pos(6))
+        self.raster_check.stateChanged.connect(self.raster_current_target)
         self.speed_slide.valueChanged.connect(self.update_speed_line)
         self.speed_line.returnPressed.connect(self.update_speed_slide)
 
@@ -77,7 +79,7 @@ class MotorControlPanel(QWidget):
         target_grid.addWidget(self.pos4_btn, 1, 0)
         target_grid.addWidget(self.pos5_btn, 1, 1)
         target_grid.addWidget(self.pos6_btn, 1, 2)
-        target_grid.addWidget(self.raster_label, 2, 0, 1, 2)
+        target_grid.addWidget(self.raster_label, 2, 0, 1, 2, Qt.AlignRight)
         target_grid.addWidget(self.raster_check, 2, 2)
 
         sub_grid = QGridLayout()
@@ -95,18 +97,48 @@ class MotorControlPanel(QWidget):
 
         self.hbox.addWidget(self.target_group)
         self.hbox.addWidget(self.sub_group)
-
+        self.hbox.addWidget(self.pos_line) # Debug
         self.setLayout(self.hbox)
 
-    def move_sub_up(self):
-        pass
+    def move_sub_up(self, pulses=1):
+        for i in range(0, pulses):
+            self.sub_pos_val += 1
+            # self.brain.move_sub(self.sub_pos_val, self.get_speed_val())
+        self.update_pos_line() # Debug
 
-    def move_sub_down(self):
-        pass
+    def move_sub_down(self, pulses=1):
+        for i in range(0, pulses):
+            self.sub_pos_val -= 1
+            # self.brain.move_sub(self.sub_pos_val, self.get_speed_val())
+        self.update_pos_line()
+
+    def get_speed_val(self):
+        return self.speed_val
+
+    def update_pos_line(self):
+        self.pos_line.setText(str(self.target_pos_val) + ':' + str(self.sub_pos_val))
 
     def update_speed_line(self):
-        pass
+        self.speed_val = self.speed_slide.value()
+        self.speed_line.setText(str(self.speed_val))
 
     def update_speed_slide(self):
+        self.speed_val = float(self.speed_line.text())
+        self.speed_slide.setValue(self.speed_val)
+
+    def set_target_pos(self, position):
+        self.target_pos_val = 100 * position  # FIXME: Fix these values
+        # self.brain.move_to_target(position)
+        self.update_pos_line()
+
+    def raster_current_target(self):
+        if self.raster_check.isChecked():
+            # self.brain.raster_target(True, self.get_current_target())
+            print('Raster On')
+        else:
+            # self.brain.raster_target(False, self.get_current_target())
+            print('Raster Off')
         pass
 
+    def get_current_target(self):
+        return self.target_pos_val
