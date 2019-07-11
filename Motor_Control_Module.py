3006ea0c723f19146148a6df898e7c7c9f2d75c2
Ian <ian.brummel@gmail.com>
2019-07-10 23:51:53 -0400

    Added manual left right control for the targets and added home routine buttons section.

--- Motor_Control_Module.py
+++ Motor_Control_Module.py
@@ -1,8 +1,11 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (QCheckBox, QHBoxLayout, QLabel, QLineEdit, QPushButton,
-                             QWidget, QGroupBox, QGridLayout, QSlider)
+                             QWidget, QGroupBox, QGridLayout, QSlider, QApplication, QFormLayout, QVBoxLayout,
+                             QToolButton)
 from pathlib import Path
+# For Testing
+import sys
 
 
 class MotorControlPanel(QWidget):
@@ -19,6 +22,25 @@ class MotorControlPanel(QWidget):
         self.raster_check = QCheckBox()
         self.raster_label = QLabel('Raster target?')
 
+        self.right_btn = QPushButton()
+        self.right_btn.setAutoRepeat(True)
+        self.right_btn.setAutoRepeatInterval(10)
+        self.right_btn.setAutoRepeatDelay(200)
+
+        self.left_btn = QPushButton()
+        self.left_btn.setAutoRepeat(True)
+        self.left_btn.setAutoRepeatInterval(10)
+        self.left_btn.setAutoRepeatDelay(200)
+
+        left_icon = QIcon()
+        right_icon = QIcon()
+        right_btn_path = Path('src/img').absolute() / 'right.svg'
+        left_btn_path = Path('src/img').absolute() / 'left.svg'
+        right_icon.addFile(str(right_btn_path))
+        left_icon.addFile(str(left_btn_path))
+        self.right_btn.setIcon(right_icon)
+        self.left_btn.setIcon(left_icon)
+
         self.tts_label = QLabel('Current TTS:')
         self.tts_line = QLineEdit()
         self.tts_line.setPlaceholderText('Go to TTS')
@@ -33,14 +55,14 @@ class MotorControlPanel(QWidget):
         self.down_sub_btn.setAutoRepeat(True)
         self.down_sub_btn.setAutoRepeatInterval(150)
         self.down_sub_btn.setAutoRepeatDelay(500)
-        self.up_icon = QIcon()
-        self.down_icon = QIcon()
-        up_btn_path = Path('src/img').absolute() / 'up_btn.svg'
-        down_btn_path = Path('src/img').absolute() / 'down_btn.svg'
-        self.up_icon.addFile(str(up_btn_path))
-        self.down_icon.addFile(str(down_btn_path))
-        self.up_sub_btn.setIcon(self.up_icon)
-        self.down_sub_btn.setIcon(self.down_icon)
+        up_icon = QIcon()
+        down_icon = QIcon()
+        up_btn_path = Path('src/img').absolute() / 'up.svg'
+        down_btn_path = Path('src/img').absolute() / 'down.svg'
+        up_icon.addFile(str(up_btn_path))
+        down_icon.addFile(str(down_btn_path))
+        self.up_sub_btn.setIcon(up_icon)
+        self.down_sub_btn.setIcon(down_icon)
 
         self.speed_val = 5
         self.speed_label = QLabel('Up/Down Speed (mm/s):')
@@ -50,12 +72,29 @@ class MotorControlPanel(QWidget):
         self.speed_slide.setTickPosition(QSlider.TicksBothSides)
         self.speed_slide.setTickInterval(1)
         self.speed_slide.setValue(self.speed_val)
-
         self.speed_line = QLineEdit(str(self.speed_val))
-        self.pos_line = QLineEdit(str(self.target_pos_val) + ':' + str(self.sub_pos_val))
+
+        self.home_sub_btn = QToolButton()
+        self.home_sub_btn.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        home_icon = QIcon()
+        home_icon.addFile(str(Path('src/img').absolute() / 'home.svg'))
+        self.home_sub_btn.setIcon(home_icon)
+        self.home_sub_btn.setText("Substrate")
+        self.home_sub_btn.setMinimumSize(64, 48)
+        self.home_target_btn = QToolButton()
+        self.home_target_btn.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        home_icon = QIcon()
+        home_icon.addFile(str(Path('src/img').absolute() / 'home.svg'))
+        self.home_target_btn.setIcon(home_icon)
+        self.home_target_btn.setText("Targets")
+        self.home_target_btn.setMinimumSize(64, 48)
 
         self.target_group = QGroupBox("Target Position:")
+        self.target_group.setMinimumSize(271, 155)
         self.sub_group = QGroupBox("Substrate Position:")
+        self.sub_group.setMinimumSize(271, 155)
+        self.home_group = QGroupBox('Home Axes:')
+        self.home_group.setMinimumSize(85, 155)
         self.hbox = QHBoxLayout()
         self.connect_controls()
         self.init_layout()
@@ -70,20 +109,28 @@ class MotorControlPanel(QWidget):
         self.pos4_btn.clicked.connect(lambda: self.set_target_pos(4))
         self.pos5_btn.clicked.connect(lambda: self.set_target_pos(5))
         self.pos6_btn.clicked.connect(lambda: self.set_target_pos(6))
+        # self.left_btn.clicked.connect(None)  # FIXME
+        # self.right_btn.clicked.connect(None)
         self.raster_check.stateChanged.connect(self.raster_current_target)
         self.speed_slide.valueChanged.connect(self.update_speed_line)
         self.speed_line.returnPressed.connect(self.update_speed_slide)
+        # self.home_target_btn.clicked.connect(None)  # FIXME
+        # self.home_sub_btn.clicked.connect(None)
 
     def init_layout(self):
+        raster_form = QFormLayout()
+        raster_form.addRow(self.raster_label, self.raster_check)
+
         target_grid = QGridLayout()
-        target_grid.addWidget(self.pos1_btn, 0, 0)
-        target_grid.addWidget(self.pos2_btn, 0, 1)
-        target_grid.addWidget(self.pos3_btn, 0, 2)
-        target_grid.addWidget(self.pos4_btn, 1, 0)
-        target_grid.addWidget(self.pos5_btn, 1, 1)
-        target_grid.addWidget(self.pos6_btn, 1, 2)
-        target_grid.addWidget(self.raster_label, 2, 0, 1, 2, Qt.AlignRight)
-        target_grid.addWidget(self.raster_check, 2, 2)
+        target_grid.addWidget(self.left_btn, 0, 0)
+        target_grid.addWidget(self.right_btn, 0, 1)
+        target_grid.addLayout(raster_form, 0, 2)
+        target_grid.addWidget(self.pos1_btn, 1, 0)
+        target_grid.addWidget(self.pos2_btn, 1, 1)
+        target_grid.addWidget(self.pos3_btn, 1, 2)
+        target_grid.addWidget(self.pos4_btn, 2, 0)
+        target_grid.addWidget(self.pos5_btn, 2, 1)
+        target_grid.addWidget(self.pos6_btn, 2, 2)
 
         sub_grid = QGridLayout()
         sub_grid.addWidget(self.tts_label, 0, 0, 1, 2)
@@ -95,31 +142,31 @@ class MotorControlPanel(QWidget):
         sub_grid.addWidget(self.speed_slide, 2, 1, 1, 2)
         sub_grid.addWidget(self.speed_line, 3, 1, 1, 2)
 
+        home_vbox = QVBoxLayout()
+        home_vbox.addWidget(self.home_target_btn)
+        home_vbox.addWidget(self.home_sub_btn)
+
         self.target_group.setLayout(target_grid)
         self.sub_group.setLayout(sub_grid)
+        self.home_group.setLayout(home_vbox)
 
         self.hbox.addWidget(self.target_group)
         self.hbox.addWidget(self.sub_group)
-        self.hbox.addWidget(self.pos_line) # Debug
+        self.hbox.addWidget(self.home_group)
         self.setLayout(self.hbox)
 
     def move_sub_up(self, pulses=1):
         for i in range(0, pulses):
             self.sub_pos_val += 1
             # self.brain.move_sub(self.sub_pos_val, self.get_speed_val())
-        self.update_pos_line() # Debug
 
     def move_sub_down(self, pulses=1):
         for i in range(0, pulses):
             self.sub_pos_val -= 1
             # self.brain.move_sub(self.sub_pos_val, self.get_speed_val())
-        self.update_pos_line()
 
     def get_speed_val(self):
         return self.speed_val
-
-    def update_pos_line(self):
-        self.pos_line.setText(str(self.target_pos_val) + ':' + str(self.sub_pos_val))
 
     def update_speed_line(self):
         self.speed_val = self.speed_slide.value()
@@ -132,7 +179,6 @@ class MotorControlPanel(QWidget):
     def set_target_pos(self, position):
         self.target_pos_val = 100 * position  # FIXME: Fix these values
         # self.brain.move_to_target(position)
-        self.update_pos_line()
 
     def raster_current_target(self):
         if self.raster_check.isChecked():
@@ -145,3 +191,20 @@ class MotorControlPanel(QWidget):
 
     def get_current_target(self):
         return self.target_pos_val
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    ex = MotorControlPanel()
+    ex.show()
+
+    sys.exit(app.exec_())
+
+
+# =============================================================================
+# Start the GUI (set up for testing for now)
+# FIXME: Need to finalize main loop for proper operation
+# =============================================================================
+if __name__ == '__main__':
+    main()
