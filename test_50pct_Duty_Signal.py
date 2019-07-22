16169a0df3fb1428222fb1ba059a6a997f4f9063
Ian <ian.brummel@gmail.com>
2019-07-22 16:11:37 -0400

    Trying testing timer method of stepping motors.

--- test_50pct_Duty_Signal.py
+++ test_50pct_Duty_Signal.py
@@ -1,5 +1,16 @@
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
+import sys
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTimer
+
+app = QApplication(sys.argv)
+
+on_timer = QTimer()
+off_timer = QTimer()
+dir_timer = QTimer()
+on_timer.setSingleShot(True)
+off_timer.setSingleShot(True)
 
 cancel = 'P9_15'
 sub_dir = "P9_19"
@@ -12,18 +23,33 @@ GPIO.setup(sub_step, GPIO.OUT)
 GPIO.output(sub_dir, GPIO.HIGH)
 sdir = 1
 
-while 1==1:
+
+def start_step():
     GPIO.output(sub_step, GPIO.HIGH)
-    sleep(0.0005)
+    off_timer.start(3)
+
+
+def finish_step():
     GPIO.output(sub_step, GPIO.LOW)
-    sleep(0.0005)
+    on_timer.start(3)
+
+
+def dir_check():
+    global sdir
+    sleep(1)
     if GPIO.input(cancel):
-        sleep(2)
-        if GPIO.input(cancel):
-            break
-        elif sdir == 1:
-            GPIO.output(sub_dir, GPIO.LOW)
-            sdir = 0
-        elif sdir == 0:
-            GPIO.output(sub_dir, GPIO.HIGH)
-            sdir = 1
+        exit()
+    elif sdir == 1:
+        GPIO.output(sub_dir, GPIO.LOW)
+        sdir = 0
+    elif sdir == 0:
+        GPIO.output(sub_dir, GPIO.HIGH)
+        sdir = 1
+
+
+on_timer.timeout.connect(start_step)
+off_timer.timeout.connect(finish_step)
+dir_timer.timeout.connect(dir_check)
+
+dir_timer.start(2)
+on_timer.start(3)
