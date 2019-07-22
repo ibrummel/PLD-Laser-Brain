a27ca6685c44fa11c183e6d2a8168d98f99d6217
Ian <ian.brummel@gmail.com>
2019-07-22 10:03:05 -0400

    Added testing ability to switch directions

--- test_50pct_Duty_Signal.py
+++ test_50pct_Duty_Signal.py
@@ -10,9 +10,20 @@ GPIO.setup(sub_dir, GPIO.OUT)
 GPIO.setup(sub_step, GPIO.OUT)
 
 GPIO.output(sub_dir, GPIO.HIGH)
+sdir = 1
 
-while not GPIO.input(cancel):
+while 1:
     GPIO.output(sub_step, GPIO.HIGH)
-    sleep(0.003)
+    sleep(0.01)
     GPIO.output(sub_step, GPIO.LOW)
-    sleep(0.003)
+    sleep(0.01)
+    if not GPIO.input(cancel):
+        sleep(2)
+        if not GPIO.input(cancel):
+            break
+        elif sdir == 1:
+            GPIO.output(sub_dir, GPIO.LOW)
+            sdir = 0
+        elif sdir == 0:
+            GPIO.output(sub_dir, GPIO.HIGH)
+            sdir = 1
